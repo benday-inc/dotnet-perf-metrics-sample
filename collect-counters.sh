04429bc1dde24831ad532f8cc12e12a5c7830b9c
benday <benday@benday.com>
2023-10-31 16:03:37 -0400

    added a note

--- collect-counters.sh
+++ collect-counters.sh
@@ -2,3 +2,10 @@
 
 dotnet-counters monitor --name Benday.PerfMetr --counters Benday.PerfMetrics.WebUi.Controllers.HomeController,Benday.PerfMetrics.WebUi.Controllers.StuffController
 
+# note: if the monitor command isn't finding your process, try this:
+# dotnet-counters ps
+#
+# that will show you the list of processes that dotnet-counters can see
+# look for the one that matches the app and then use that name.
+# it can be a little confusing because it seems like the tool goes after an abreviated name
+# sometimes.  
